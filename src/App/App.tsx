ebab8fe97cb1921e2315ccc22f3b427dfeaa263e
Awangku Muhammad Syazwan bin Awangku Sazali <aasazali@seekasia.com>
2025-02-11 14:51:01 +0800

    Separate out online and print resume

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,11 +1,11 @@
 import 'braid-design-system/reset';
 
-import { BraidProvider } from 'braid-design-system';
+import { BraidProvider, Hidden } from 'braid-design-system';
 import seekJobs from 'braid-design-system/themes/seekJobs';
 import { StrictMode } from 'react';
 
-import Resume from './Resume';
-
+import OnlineResume from './OnlineResume';
+import PaperResume from './PaperResume';
 interface AppProps {
   environment: 'development' | 'production';
 }
@@ -13,7 +13,12 @@ interface AppProps {
 export default ({ environment }: AppProps) => (
   <StrictMode>
     <BraidProvider theme={seekJobs}>
-      <Resume />
+      <Hidden print>
+        <OnlineResume />
+      </Hidden>
+      <Hidden screen>
+        <PaperResume />
+      </Hidden>
     </BraidProvider>
   </StrictMode>
 );
---
+++ src/App/PaperResume.tsx
@@ -0,0 +1,20 @@
+import { PageBlock, Box } from 'braid-design-system';
+
+import { ResumeIsWIP } from '../components/Notices/ResumeIsWIP';
+import { PageContent } from '../components/PageSections/PageContent';
+import { PageFooter } from '../components/PageSections/PageFooter';
+import PageHeader from '../components/PageSections/PageHeader';
+
+export const PaperResume = () => (
+  <PageBlock component="main" width="medium">
+    <Box height="touchable" />
+    <ResumeIsWIP />
+    <PageHeader />
+    <PageContent />
+    <Box height="touchable" />
+    <PageFooter />
+    <Box height="touchable" />
+  </PageBlock>
+);
+
+export default PaperResume;
